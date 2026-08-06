fabb03f7ab85367db7c22de61a26d1377509ff8b
Diego Alves <diegoarpalves@gmail.com>
2026-08-06 22:54:26 +0100

    Add Firebase configuration

---
+++ app/firebase_config.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+firebase_config = {
+    "apiKey": "AIzaSyBp83M0d78FuUE9vYJV74YhgsmjiBYK-7A",
+    "authDomain": "yaffiliate-4fe3e.firebaseapp.com",
+    "projectId": "yaffiliate-4fe3e",
+    "storageBucket": "yaffiliate-4fe3e.firebasestorage.app",
+    "messagingSenderId": "178143274250",
+    "appId": "1:178143274250:web:4cd1f7d8a56d85cf6446f3",
+}
